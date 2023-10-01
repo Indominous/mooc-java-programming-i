71050ea6d3d0727dff4f20d08cfaad0522b6b08e
Indominous <63386602+Indominous@users.noreply.github.com>
2023-10-01 15:10:09 +0530

    boolean input sol

--- part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
+++ part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
@@ -5,6 +5,9 @@ public class BooleanInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Write something: ");
+        boolean BooleanInput = Boolean.valueOf(scanner.nextLine());
+        System.out.println("True or false? " + BooleanInput);
 
         // write your program here
 
