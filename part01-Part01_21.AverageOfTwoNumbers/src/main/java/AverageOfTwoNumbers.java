fe31fe33aa7435c2a221a9ccd9a4a7677c42fbbf
Indominous <63386602+Indominous@users.noreply.github.com>
2023-10-05 23:16:52 +0530

    avg of two numbers sol

--- part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
+++ part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
@@ -7,6 +7,13 @@ public class AverageOfTwoNumbers {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
+        int sumoftwonumbers = first + second;
+        double AverageOfTwoNumbers =  1.0 * sumoftwonumbers / 2;
+        System.out.println("The average is " + AverageOfTwoNumbers);
 
     }
 }
