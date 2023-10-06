c85f730c9f79f9bfe56499ddaa4aa408d12440f9
Indominous <63386602+Indominous@users.noreply.github.com>
2023-10-06 14:53:06 +0530

    simple calc sol

--- part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -7,6 +7,22 @@ public class SimpleCalculator {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give the first number:");
+        int firstnum = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int secondnum = Integer.valueOf(scanner.nextLine());
+        int sum = firstnum + secondnum;
+        int diff = firstnum - secondnum;
+        int product = firstnum * secondnum;
+        double quotient = 1.0 * firstnum / secondnum;
+        System.out.println(firstnum + " + " + secondnum + " = " +sum );
+        System.out.println(firstnum + " - " + secondnum + " = " +diff );
+        System.out.println(firstnum + " * " + secondnum + " = " +product );
+        System.out.println(firstnum + " / " + secondnum + " = " +quotient );
+        
+        
+
+        
 
     }
 }
