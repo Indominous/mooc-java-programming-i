fcd6768d56c4a4f2297c96cf6c3920a78f0e615c
Indominous <63386602+Indominous@users.noreply.github.com>
2023-10-09 08:07:23 +0530

    "same" sol

--- part01-Part01_34.Same/src/main/java/Same.java
+++ part01-Part01_34.Same/src/main/java/Same.java
@@ -7,5 +7,15 @@ public class Same {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here. 
+        System.out.println("Enter the first string:");
+        String firsString = scan.nextLine();
+        System.out.println("Enter the second string:");
+        String secString = scan.nextLine();
+        if(firsString.equals(secString)){
+            System.out.println("Same");
+        }else{
+            System.out.println("Different");
+        }
+        scan.close();
     }
 }
