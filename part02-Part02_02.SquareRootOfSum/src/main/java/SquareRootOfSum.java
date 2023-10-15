4230fcd94723b9f310d9a87074f94f612d08bfe3
Indominous <63386602+Indominous@users.noreply.github.com>
2023-10-15 12:45:19 +0530

    "sqaure root of sum" sol

--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -5,6 +5,13 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give a second number:");
+        int second = Integer.valueOf(scanner.nextLine());
+        int sum = (first + second);
+        double squareRootOfSum = Math.sqrt(sum);
+        System.out.println(squareRootOfSum);
 
     }
 }
