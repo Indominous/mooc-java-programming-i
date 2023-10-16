3037dea17e873433b2d26d8c8e9bcadc1568baa8
Indominous <63386602+Indominous@users.noreply.github.com>
2023-10-16 22:54:07 +0530

    "are we there yet" sol

--- part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
+++ part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
@@ -5,6 +5,9 @@ public class AreWeThereYet {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while () {
+            
+        }
 
     }
 }
