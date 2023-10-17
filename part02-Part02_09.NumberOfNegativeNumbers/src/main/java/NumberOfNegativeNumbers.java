98b22aa98b7b62e223fbe6156cfe519b8f739733
Indominous <63386602+Indominous@users.noreply.github.com>
2023-10-17 23:11:01 +0530

    number of neg numbers sol

--- part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
+++ part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
@@ -5,6 +5,22 @@ public class NumberOfNegativeNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int num = 0;
+        int numberofnegnums = 0;
+        while (true) {
+            System.out.println("Give a number: ");   
+            num = Integer.valueOf(scanner.nextLine());
+            if(num == 0){
+                break;
+            }else if(num > 0){
+                continue;
+            }else if(num < 0){
+                numberofnegnums = numberofnegnums + 1;
+            }
 
+
+        }
+        System.out.println("Number of negative numbers: " + numberofnegnums);
+        scanner.close();
     }
 }
