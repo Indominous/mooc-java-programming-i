faf2e5b5ceab53c1c74c365e6fd3489bc56a2049
Indominous <63386602+Indominous@users.noreply.github.com>
2023-10-18 07:11:36 +0530

    "avg of nums" sol

--- part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,6 +5,21 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        double sum = 0;
+        double numofnums = 0;
+        double num = 0;
+        while (true) {
+            System.out.println("Give a number: ");
+            num = Integer.valueOf(scanner.nextLine());
+            if (num == 0) {
+                break;
+            } else {
+                numofnums = numofnums +1;
+                sum = sum + num;
+            }
+        }
+        double average = sum / numofnums;
+        System.out.println("Average of the numbers: " + average);
+        scanner.close();
     }
 }
