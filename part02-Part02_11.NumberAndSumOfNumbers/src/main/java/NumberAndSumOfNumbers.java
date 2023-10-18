88bcde4080f17da99a86d61eaf2033c8ca4822c3
Indominous <63386602+Indominous@users.noreply.github.com>
2023-10-18 07:05:36 +0530

    "num and sum of nums" sol

--- part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
+++ part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
@@ -2,9 +2,23 @@
 import java.util.Scanner;
 
 public class NumberAndSumOfNumbers {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int sum = 0;
+        int numofnums = 0;
+        int num = 0;
+        while (true) {
+            System.out.println("Give a number: ");
+            num = Integer.valueOf(scanner.nextLine());
+            if (num == 0) {
+                break;
+            } else {
+                numofnums = numofnums +1;
+                sum = sum + num;
+            }
+        }
+        System.out.println("Number of numbers: " + numofnums);
+        System.out.println("Sum of the numbers: " + sum);
+        scanner.close();
     }
 }
